522d35a66ed06960edd540afeecf5a69b68a0da4
Deb Totten <deborahrtotten@gmail.com>
2018-09-18 21:55:16 -0400

    submitting an in-progress copy

--- public/app.js
+++ public/app.js
@@ -1,73 +1,116 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
-  });
-  
-  
-  // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
-    // Empty the notes from the note section
-    $("#notes").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
-  
-    // Now make an ajax call for the Article
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .then(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
-        }
-      });
-  });
-  
-  // When you click the savenote button
-  $(document).on("click", "#savenote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
+
+$(document).ready(function() {
+  var articleContainer = $(".article-container");
+  $(document).on("click", ".btn.save", handleArticleSave);
+  $(document).on("click", ".scrape-new", handleArticleScrape);
+  $(".clear").on("click", handleArticleClear);
+
+  function initPage() {
+    $.get("/api/headlines?saved=false").then(function(data) {
+      articleContainer.empty();
+      if (data && data.length) {
+        renderArticles(data);
+      } else {
+        renderEmpty();
       }
-    })
-      // With that done
-      .then(function(data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
-  
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
-  
+    });
+  }
+
+  function renderArticles(articles) {
+    var articleCards = [];
+    for (var i = 0; i < articles.length; i++) {
+      articleCards.push(createCard(articles[i]));
+    }
+    articleContainer.append(articleCards);
+  }
+
+  function createCard(article) {
+    var card = $("<div class='card'>");
+    var cardHeader = $("<div class='card-header'>").append(
+      $("<h3>").append(
+        $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
+          .attr("href", article.url)
+          .text(article.headline),
+        $("<a class='btn btn-success save'>Save Article</a>")
+      )
+    );
+
+    var cardBody = $("<div class='card-body'>").text(article.summary);
+
+    card.append(cardHeader, cardBody);
+    // We attach the article's id to the jQuery element
+    // We will use this when trying to figure out which article the user wants to save
+    card.data("_id", article._id);
+    // We return the constructed card jQuery element
+    return card;
+  }
+
+  function renderEmpty() {
+    // This function renders some HTML to the page explaining we don't have any articles to view
+    // Using a joined array of HTML string data because it's easier to read/change than a concatenated string
+    var emptyAlert = $(
+      [
+        "<div class='alert alert-warning text-center'>",
+        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+        "</div>",
+        "<div class='card'>",
+        "<div class='card-header text-center'>",
+        "<h3>What Would You Like To Do?</h3>",
+        "</div>",
+        "<div class='card-body text-center'>",
+        "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+        "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+        "</div>",
+        "</div>"
+      ].join("")
+    );
+    // Appending this data to the page
+    articleContainer.append(emptyAlert);
+  }
+
+  function handleArticleSave() {
+    // This function is triggered when the user wants to save an article
+    // When we rendered the article initially, we attached a javascript object containing the headline id
+    // to the element using the .data method. Here we retrieve that.
+    var articleToSave = $(this)
+      .parents(".card")
+      .data();
+
+    // Remove card from page
+    $(this)
+      .parents(".card")
+      .remove();
+
+    articleToSave.saved = true;
+    // Using a patch method to be semantic since this is an update to an existing record in our collection
+    $.ajax({
+      method: "PUT",
+      url: "/api/headlines/" + articleToSave._id,
+      data: articleToSave
+    }).then(function(data) {
+      // If the data was saved successfully
+      if (data.saved) {
+        // Run the initPage function again. This will reload the entire list of articles
+        initPage();
+      }
+    });
+  }
+
+  function handleArticleScrape() {
+    // This function handles the user clicking any "scrape new article" buttons
+    $.get("/api/fetch").then(function(data) {
+      // If we are able to successfully scrape the NYTIMES and compare the articles to those
+      // already in our collection, re render the articles on the page
+      // and let the user know how many unique articles we were able to save
+      initPage();
+      bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
+    });
+  }
+
+  function handleArticleClear() {
+    $.get("api/clear").then(function() {
+      articleContainer.empty();
+      initPage();
+    });
+  }
+});
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+// var db = require("./models/article");
 
 var PORT = 3000;
 
@@ -41,12 +41,12 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.timesdispatch.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h3.tnt-headline").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
@@ -73,7 +73,7 @@ app.get("/scrape", function(req, res) {
     // If we were able to successfully scrape and save an Article, send a message to the client
     res.send("Scrape Complete");
   });
-});
+});+
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
@@ -105,27 +105,8 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
 
-// Start the server
+Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
